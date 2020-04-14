e4790f294832cf75d096f246d36c8e7309aae0c7
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-04-14 17:03:38 +0200

    -aexo3 bnb onglets du bas

--- bnb/src/app/app-routing.module.ts
+++ bnb/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'logements', pathMatch: 'full' },
   // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'auth',
