a77605f1967ca985e068663fb95f614934553cea
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-04-15 16:46:32 +0200

    -aexo3 bnb v0.4

--- bnb/src/app/logements/offres/edit-offre/edit-offre.page.ts
+++ bnb/src/app/logements/offres/edit-offre/edit-offre.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Logement } from '../../logement.model';
+import { LogementsService } from '../../logements.service';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-offre',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offre.page.scss'],
 })
 export class EditOffrePage implements OnInit {
+   
+  logement: Logement;
 
-  constructor() { }
+  constructor(private logementsService: LogementsService,
+              private route: ActivatedRoute,
+              private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('logementId')) {
+        this.navCtrl.navigateBack('/logements/tabs/offres');
+        return;
+      }
+      this.logement = this.logementsService.getLogement(paramMap.get('logementId'));
+    })
   }
-
 }
